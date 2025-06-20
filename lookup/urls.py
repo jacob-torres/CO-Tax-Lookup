a29a28358a9bdecdc65c5377476126dc4fbf138b
Jacob Torres <contact@jacobtorres.net>
2025-06-20 12:17:18 -0600

    Created initial lookup index URL and view function; no UI yet.

--- lookup/urls.py
+++ lookup/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('/', view=views.index, name='index')
+    path('', views.index, name='index')
 ]
--- lookup/views.py
+++ lookup/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    """Index view function for lookup application.
+
+    params:
+    -------
+    request: HTTP request data
+
+    returns:
+    --------
+    django.http.HttpResponse object
+    """
+
+    return HttpResponse("Hello CO Tax Lookup!")
